82ac990b97383fd4a8c2d17e78a2af4dfac25ff1
Juanf <juanfa107@gmail.com>
2020-06-25 23:02:36 -0500

    validaciones form

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,7 @@
+// tslint:disable-next-line: class-name
+
+export class usuarioModel {
+    email: string;
+    password: string;
+    nombre: string;
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { usuarioModel } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  usuario: usuarioModel = new usuarioModel();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(form: NgForm){
+    console.log(form)
   }
 
 }
